511ad433735aab20a1562c4b4f3d39549c982321
Benjamin Gelb <beg5670@gmail.com>
2019-04-20 19:41:15 +0700

    abstract out common functinoality for udp and tcp scans

--- lib/scan.ml
+++ lib/scan.ml
@@ -1,17 +1,24 @@
+(* look into these libraries for creating
+ * custom packets to do more advanced port scans:
+ *   - https://github.com/mirage/mirage-tcpip
+ *   - https://github.com/frenetic-lang/ocaml-packet
+ *   - https://mirage.io/blog/intro-tcpip
+ *)
+
 open Core
 
 type probe_type =
   | TCP_Connect
   | TCP_Syn
   | UDP
-  
+
 type scan_config = {
     host: string;
     min_port: int;
     max_port: int;
     probe_type: probe_type;
   }
-                 
+
 type port_status =
   | Open
   | Closed
@@ -26,8 +33,9 @@ type scan_result = {
 let probe_type_of_string = function
   | "tcp-syn" -> TCP_Syn
   | "udp" -> UDP
-  | "tcp-connect" | _ -> TCP_Connect
-           
+  | "tcp-connect" -> TCP_Connect
+  | _ -> failwith "Invalid probe selected"
+                   
 let string_of_port_status = function
   | Open -> "open"
   | Closed -> "closed"
@@ -42,31 +50,68 @@ let print_scan_result result =
   ANSITerminal.printf [color] "[+] Port [%d] is [%s]\n"
     result.port (string_of_port_status result.status)
 
-let tcp_connect_probe host port =
-  let inet_address = Unix.Inet_addr.of_string_or_getbyname host in
-  let socket_address = Unix.ADDR_INET (inet_address, port) in
-  let socket = Unix.socket ~domain:Unix.PF_INET ~kind:Unix.SOCK_STREAM ~protocol:0 in
+let create_socket host port probe_type =
+    let inet_address = Unix.Inet_addr.of_string_or_getbyname host in
+    let socket_address = Unix.ADDR_INET (inet_address, port) in
+    let socket = match probe_type with
+      | TCP_Syn | TCP_Connect -> Unix.socket
+                                   ~domain:Unix.PF_INET
+                                   ~kind:Unix.SOCK_STREAM
+                                   ~protocol:0
+      | UDP -> Unix.socket
+                 ~domain:Unix.PF_INET
+                 ~kind:Unix.SOCK_DGRAM
+                 ~protocol:(UnixLabels.getprotobyname "udp").p_proto
+    in
+    (socket, socket_address)
+      
+let connect ~probe_fn ~host ~port ~probe_type =  
+  let socket, socket_address = create_socket host port probe_type in
+
   try
     Unix.connect socket ~addr:socket_address;
+    probe_fn socket socket_address;
     Unix.shutdown socket ~mode:Unix.SHUTDOWN_ALL;
     Open;
   with
   | Unix.Unix_error (Unix.ECONNREFUSED, _, _) -> Closed
   | Unix.Unix_error (e, _, _) -> Error e
-
-(* Use _ prefix to supress unsued warnings since this is not implemented yet *)
+    
+(* Use `_` prefix to supress unsued warnings since this is not implemented yet *)
 let tcp_syn_probe _host _port = Open
+                              
+let tcp_connect_probe host port = 
+  connect
+    ~host:host
+    ~port:port
+    ~probe_type:TCP_Connect
+    ~probe_fn:(fun _ _ -> ())
 
-(* Use _ prefix to supress unsued warnings since this is not implemented yet *)
-let udp_probe _host _port = Open 
-              
+let udp_probe host port =
+  connect
+    ~host:host
+    ~port:port
+    ~probe_type:UDP
+    ~probe_fn:(fun socket socket_address ->
+      let msg = "\x00" in
+        Unix.sendto
+           socket
+           ~buf:(Bytes.of_string msg)
+           ~pos:0
+           ~len:(String.length msg)
+           ~mode:[]
+           ~addr:socket_address
+        |> ignore
+      
+    )
+
 let probe_type_fn = function
   | TCP_Connect -> tcp_connect_probe
   | TCP_Syn -> tcp_syn_probe
   | UDP -> udp_probe
 
 let initialize config =
-  (* recursion would be nice, but does it make sense to use here? *)
+  (* recursion would be nice, but it doesn't make sense to use here *)
   let probe_fn = probe_type_fn config.probe_type in
   for port = config.min_port to config.max_port do
     let status = probe_fn config.host port in
